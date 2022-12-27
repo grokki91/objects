375fd138de91b503f0dff8565ccd292fa7af573e
Pavel Chekalin <grokki91@gmail.com>
2022-12-27 23:48:30 +0300

    Fix eslint warning

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 export default function orderByProps(obj, arr) {
   const firstArr = [];
   const secondArr = [];
-  let firstObj = {};
   for (const key in obj) {
-    const secondObj = { key, value: obj[key] };
-    if (arr) {
-      arr.forEach((el) => {
-        if (el === key) {
-          firstObj = { key, value: obj[key] };
-          firstArr.push(firstObj);
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      const secondObj = { key, value: obj[key] };
+      if (arr) {
+        arr.forEach((el) => {
+          if (el === key) {
+            let firstObj = {};
+            firstObj = { key, value: obj[key] };
+            firstArr.push(firstObj);
+          }
+        });
+        if (!arr.includes(key)) {
+          secondArr.push(secondObj);
         }
-      });
-      if (!arr.includes(key)) {
-        secondArr.push(secondObj);
       }
+      secondArr.sort((a, b) => b.value - a.value);
     }
-    secondArr.sort((a, b) => b.value - a.value);
   }
 
   return [...firstArr, ...secondArr];
