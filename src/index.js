605c914737b0e7eb49cc23a97a6c869f3faeddba
Pavel Chekalin <grokki91@gmail.com>
2023-01-03 20:47:11 +0300

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
 export default function orderByProps(obj, arr) {
   const firstArr = [];
   const secondArr = [];
-  for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      const secondObj = { key, value: obj[key] };
-      if (arr) {
-        arr.forEach((el) => {
-          if (el === key) {
-            let firstObj = {};
-            firstObj = { key, value: obj[key] };
-            firstArr.push(firstObj);
-          }
-        });
-        if (!arr.includes(key)) {
-          secondArr.push(secondObj);
-        }
+  const objKeys = Object.keys(obj);
+  for (const key of objKeys) {
+    const secondObj = { key, value: obj[key] };
+    arr.forEach((el) => {
+      if (el === key) {
+        const firstObj = { key, value: obj[key] };
+        firstArr.push(firstObj);
       }
-      secondArr.sort((a, b) => b.value - a.value);
+    });
+    if (!arr.includes(key)) {
+      secondArr.push(secondObj);
     }
   }
+  secondArr.sort((a, b) => b.value - a.value);
 
   return [...firstArr, ...secondArr];
 }
